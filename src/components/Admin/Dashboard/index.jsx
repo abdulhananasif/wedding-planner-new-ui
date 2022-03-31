bebe3c8319e578438a2af526903967856f6b5a36
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2022-03-31 13:55:55 +0500

    Feat: Remaining height calculated for admin dashboard

--- src/components/Admin/Dashboard/index.jsx
+++ src/components/Admin/Dashboard/index.jsx
@@ -3,9 +3,21 @@ import Sidebar from "./Sidebar";
 
 const AdminDashboard = () => {
   const [sidebarHeight, setSidebarHeight] = useState();
+  const [dashboardHeight, setDashboardHeight] = useState();
+  const [remainingHeight, setRemainingHeight] = useState();
+  const ref = useRef(null);
+  useEffect(() => {
+    setDashboardHeight(ref.current.clientHeight);
+  }, []);
+  useEffect(() => {
+    if (sidebarHeight && dashboardHeight)
+      setRemainingHeight(dashboardHeight - sidebarHeight);
+  }, [dashboardHeight, sidebarHeight]);
   return (
     <>
-      <Sidebar setSidebarHeight={setSidebarHeight} />
+      <div ref={ref} style={{ height: "100vh" }}>
+        <Sidebar setSidebarHeight={setSidebarHeight} />
+      </div>
     </>
   );
 };
