fe0fb613cf4233a5e972087f9fede926a4ae2996
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2022-03-31 15:58:00 +0500

    Revert "Feat: Sidebar items icons added"
    This reverts commit bab63616294bd75be7adc0ecdcfc38ca4e8d6196.

--- src/components/Admin/Dashboard/Sidebar/index.jsx
+++ src/components/Admin/Dashboard/Sidebar/index.jsx
@@ -5,10 +5,6 @@ import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import AddIcon from "@mui/icons-material/Add";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import LogoutIcon from "@mui/icons-material/Logout";
 
 const Sidebar = ({ setSidebarHeight }) => {
   const [state, setState] = useState(false);
@@ -40,25 +36,15 @@ const Sidebar = ({ setSidebarHeight }) => {
       onKeyDown={toggleDrawer(false)}
     >
       <List>
-        {["Add", "View"].map((text, index) => (
+        {["Add", "View"].map((text) => (
           <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? (
-                <AddIcon sx={{ width: "100%" }} />
-              ) : (
-                <VisibilityIcon sx={{ width: "100%" }} />
-              )}
-            </ListItemIcon>
-            <ListItemText sx={{ marginLeft: "7px" }} primary={text} />
+            <ListItemText primary={text} />
           </ListItem>
         ))}
       </List>
       <List>
         <ListItem button key={"Logout"}>
-          <ListItemIcon>
-            <LogoutIcon sx={{ width: "100%" }} />
-          </ListItemIcon>
-          <ListItemText sx={{ marginLeft: "7px" }} primary={"Logout"} />
+          <ListItemText primary={"Logout"} />
         </ListItem>
       </List>
     </Box>
